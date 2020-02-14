553a363e6d4030fe1130afc4a5607e87663d24e9
Olya <lelia.dragon13@gmail.com>
2020-02-14 21:43:39 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+function plus() {
+
+  var num1, num2, result;
+  num1 = document.getElementById('num1').value;
+  num1 = parseInt(num1);
+  
+  
+  num2 = document.getElementById('num2').value;
+  num2 = parseInt(num2);
+   
+  
+  result = num1 + num2;
+  document.getElementById('result').innerHTML = ' = ' + result;
+  
+}
+
+function minus() {
+  var num1, num2, result;
+  num1 = document.getElementById('num1').value;
+  num1 = parseInt(num1);
+  
+  
+  num2 = document.getElementById('num2').value;
+  num2 = parseInt(num2);
+   
+  
+  result = num1 - num2;
+  document.getElementById('result').innerHTML = ' = ' + result;
+
+}
+
+function multiply() {
+  var num1, num2, result;
+  num1 = document.getElementById('num1').value;
+  num1 = parseInt(num1);
+  
+  
+  num2 = document.getElementById('num2').value;
+  num2 = parseInt(num2);
+  
+  result = num1 * num2;
+  document.getElementById('result').innerHTML = ' = ' + result;
+  
+}
+
+function division() {
+  var num1, num2, result;
+  num1 = document.getElementById('num1').value;
+  num1 = parseInt(num1);
+  
+  
+  num2 = document.getElementById('num2').value;
+  num2 = parseInt(num2);
+  
+  result = num1 / num2;
+  document.getElementById('result').innerHTML = ' = ' + result;
+}
